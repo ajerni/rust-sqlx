d1a7489001376f86d0ded1484f9f770c92853162
„Andi <ajerni@gmail.com>
2024-03-17 14:03:24 +0100

    Actix added and CRUD implemented

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,94 @@
+use actix_web::{get, post, delete, patch, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
-use sqlx::{types::Json, FromRow, Row};
+use sqlx::{types::Json, FromRow, PgPool, Row};
 use std::env;
 use std::error::Error;
 
-#[derive(Debug, FromRow, Clone)]
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[post("/create")]
+async fn create(pool: web::Data<PgPool>, book: web::Json<Book>) -> impl Responder {
+    match create_d(&book, &pool).await {
+        Ok(message) => HttpResponse::Ok().body(message),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+    }
+}
+
+#[get("/books")]
+async fn get_all_books(pool: web::Data<PgPool>) -> impl Responder {
+    let q = "SELECT isbn, title, author, metadata FROM book";
+    let rows = match sqlx::query(q).fetch_all(pool.get_ref()).await {
+        Ok(rows) => rows,
+        Err(e) => {
+            return HttpResponse::InternalServerError().body(format!("Database error: {}", e))
+        }
+    };
+
+    let books = rows
+        .iter()
+        .map(|row| Book {
+            isbn: row.get("isbn"),
+            title: row.get("title"),
+            author: row.get("author"),
+            metadata: row.get("metadata"),
+        })
+        .collect::<Vec<Book>>();
+
+    HttpResponse::Ok().json(books)
+}
+
+#[get("/book/{isbn}")]
+async fn get_book_by_id(pool: web::Data<PgPool>, path: web::Path<(String,)>) -> impl Responder {
+    let isbn = path.into_inner().0;
+    match get_book_d(&isbn, &pool).await {
+        Ok(Some(book)) => HttpResponse::Ok().json(book),
+        Ok(None) => HttpResponse::NotFound().body("Book not found."),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+    }
+}
+
+#[patch("/update/{isbn}")]
+async fn update_book(pool: web::Data<PgPool>, path: web::Path<(String,)>, new_book: web::Json<Book>) -> impl Responder {
+    let isbn = path.into_inner().0;
+    match get_book_d(&isbn, &pool).await {
+        Ok(Some(mut book)) => {
+            book.title = new_book.title.clone();
+            book.author = new_book.author.clone();
+            book.metadata = new_book.metadata.clone();
+            
+            match update_d(&book, &pool).await {
+                Ok(_) => HttpResponse::Ok().body("Book updated successfully."),
+                Err(e) => HttpResponse::InternalServerError().body(format!("Error updating book: {}", e)),
+            }
+        },
+        Ok(None) => HttpResponse::NotFound().body("Book not found"),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+    }
+}
+
+#[delete("/delete/{isbn}")]
+async fn delete_book(pool: web::Data<PgPool>, path: web::Path<(String,)>) -> impl Responder {
+    let isbn = path.into_inner().0;
+    match delete_d(&isbn, &pool).await {
+        Ok(_) => HttpResponse::Ok().body("Book deleted successfully."),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error deleting book: {}", e)),
+    }
+}
+
+#[derive(Debug, FromRow, Clone, Serialize, Deserialize)]
 struct Book {
     pub isbn: String,
     pub title: String,
@@ -18,7 +102,10 @@ struct Metadata {
     pub tags: Vec<String>,
 }
 
-async fn create(book: &Book, pool: &sqlx::PgPool) -> Result<&'static str, Box<dyn Error>> {
+// The following methods (_d for direct) are also for direct use with sqlx / all off them are
+// separately implementet or called from the server above (Main diff: sqlx::Pool vs. web::Data<PgPool>)
+
+async fn create_d(book: &Book, pool: &sqlx::PgPool) -> Result<&'static str, Box<dyn Error>> {
     let query = "INSERT INTO book (isbn, title, author, metadata) VALUES ($1, $2, $3, $4)";
 
     let mut book_to_insert = book.clone(); // Clone the book to avoid modifying the original
@@ -41,7 +128,7 @@ async fn create(book: &Book, pool: &sqlx::PgPool) -> Result<&'static str, Box<dy
     Ok("Book created successfully.")
 }
 
-async fn get_book(isbn: &str, pool: &sqlx::PgPool) -> Result<Option<Book>, Box<dyn Error>> {
+async fn get_book_d(isbn: &str, pool: &sqlx::PgPool) -> Result<Option<Book>, Box<dyn Error>> {
     let book = sqlx::query_as::<_, Book>(
         "SELECT book.isbn, book.title, book.author, book.metadata FROM book WHERE isbn = $1",
     )
@@ -52,7 +139,7 @@ async fn get_book(isbn: &str, pool: &sqlx::PgPool) -> Result<Option<Book>, Box<d
     Ok(book)
 }
 
-async fn get_all_books(pool: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>> {
+async fn get_all_books_d(pool: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>> {
     let q = "SELECT isbn, title, author, metadata FROM book";
     let rows = sqlx::query(q).fetch_all(pool).await?;
 
@@ -69,7 +156,10 @@ async fn get_all_books(pool: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>>
     Ok(books)
 }
 
-async fn get_books_by_author(author: &str, pool: &sqlx::PgPool) -> Result<Vec<Book>, Box<dyn Error>> {
+async fn get_books_by_author_d(
+    author: &str,
+    pool: &sqlx::PgPool,
+) -> Result<Vec<Book>, Box<dyn Error>> {
     let q = "SELECT isbn, title, author, metadata FROM book WHERE author = $1";
     let rows = sqlx::query(q).bind(author).fetch_all(pool).await?;
 
@@ -86,7 +176,7 @@ async fn get_books_by_author(author: &str, pool: &sqlx::PgPool) -> Result<Vec<Bo
     Ok(books)
 }
 
-async fn update(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+async fn update_d(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
     let query = "UPDATE book SET title = $1, author = $2, metadata = $3 WHERE isbn = $4";
 
     sqlx::query(query)
@@ -100,7 +190,7 @@ async fn update(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> 
     Ok(())
 }
 
-async fn delete(isbn: &str, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+async fn delete_d(isbn: &str, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
     let query_string = "DELETE FROM book WHERE isbn = $1";
 
     sqlx::query(query_string).bind(isbn).execute(pool).await?;
@@ -108,11 +198,27 @@ async fn delete(isbn: &str, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-#[tokio::main]
+#[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env");
     let pool = sqlx::postgres::PgPool::connect(&url.as_str()).await?;
+
+    HttpServer::new(move|| {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(hello)
+            .service(echo)
+            .service(create)
+            .service(get_all_books)
+            .service(get_book_by_id)
+            .service(update_book)
+            .service(delete_book)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
 
     //sqlx::migrate!("./migrations").run(&pool).await?;
 
@@ -126,24 +232,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //     })),
     // };
 
-    // let successful_created = create(&book, &pool).await?;
+    // let successful_created = create_d(&book, &pool).await?;
     // println!("{}", successful_created);
 
-    // let book: Option<Book> = get_book("999", &pool).await?;
+    // let book: Option<Book> = get_book_d("999", &pool).await?;
     // match book {
     //     Some(b) => println!("Book: {:#?}", b),
     //     None => println!("Book not found."),
     // }
 
-    let books: Vec<Book> = get_all_books(&pool).await?;
-    println!("Books: {:?}", books);
+    // let books: Vec<Book> = get_all_books_d(&pool).await?;
+    // println!("Books: {:?}", books);
 
-    // let books: Vec<Book> = get_books_by_author("Andi Erni", &pool).await?;
+    // let books: Vec<Book> = get_books_by_author_d("Andi Erni", &pool).await?;
     // println!("Books: {:#?}", books);
 
-    // update(&book, &pool).await?;
+    // update_d(&book, &pool).await?;
 
-    // delete("1111", &pool).await?;
+    // delete_d("1111", &pool).await?;
 
     Ok(())
 }

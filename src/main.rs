acb930952b4c63f5fd5b165a167c00cf98ac32c0
„Andi <ajerni@gmail.com>
2024-04-11 14:46:20 +0200

    changes

--- src/main.rs
+++ src/main.rs
@@ -297,10 +297,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .allowed_origin("https://rust-sqlx.onrender.com")
             .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".onrender.com"))
             .allowed_origin("https://bevy.andierni.ch")
-            .allowed_origin("https://htmx.andierni.ch/")
+            .allowed_origin("https://htmx.andierni.ch")
             .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".andierni.ch"))
             .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
-            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT, header::ACCESS_CONTROL_ALLOW_ORIGIN])
             .allowed_header(header::CONTENT_TYPE)
             .max_age(3600);
         App::new()

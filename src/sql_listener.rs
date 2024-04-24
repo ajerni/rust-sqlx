432ab692075ae5d40790f0d24710fa9bf421b8b6
„Andi <ajerni@gmail.com>
2024-04-24 12:09:38 +0200

    tempdir added and dweet done

--- src/sql_listener.rs
+++ src/sql_listener.rs
@@ -1,12 +1,33 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use sqlx::PgPool;
 use sqlx::postgres::PgListener;
-
 use webbrowser::open;
 
-fn go_to_page() {
+fn _go_to_page() {
   let url = "https://www.andierni.ch";
   open(url).unwrap();
+}
+
+fn open_message_box() {
+    let html = r#"
+        <html>
+            <body>
+                <h1>Data entered in DB</h1>
+                <p>This temporary file was created uppon notification from PostgreSQL.</p>
+                <button onclick="window.close()">OK</button>
+            </body>
+        </html>
+    "#;
+
+    // creates a temporary file on the heap (so it is automatically deleted again when it goes out of scope)
+    let dir = tempfile::TempDir::new().unwrap();
+    let file_path = dir.path().join("message.html");
+    std::fs::write(&file_path, html).unwrap();
+
+    webbrowser::open(&format!("file://{}", file_path.display())).unwrap();
+
+    // Keep the temporary file around for 1 second
+    std::thread::sleep(std::time::Duration::from_secs(1));
 }
 
 #[get("/listen")]
@@ -37,10 +58,12 @@ async fn handle_notification(event: sqlx::postgres::PgNotification) {
     println!("Notification successfully received: {:?}", event);
     println!("Got this message: {}", event.payload());
     if event.payload() == "plpgsql-trigger" {
-        println!("plpsql is soooo coool!");
+        println!("plpgql is soooo coool!");
     }
     
-    //TODO: use WebSockets or Server Sent Events to trigger reload of fetch() in htmx.html
-    go_to_page();
+    //TODO: use WebSockets or Server Sent Events to trigger reload of fetch() in htmx.html (so far polling with htmx every 2s is used)
+    
+    //go_to_page();
+    open_message_box();
     
 }
